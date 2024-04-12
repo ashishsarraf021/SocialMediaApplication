6e01707f913ad8a9acf7162a9e36d3591176253e
Ashish Sarraf <93375826+ashishsarraf021@users.noreply.github.com>
2024-04-12 22:39:29 +0530

    Update PostWidget.jsx

--- client/src/scenes/widgets/PostWidget.jsx
+++ client/src/scenes/widgets/PostWidget.jsx
@@ -36,7 +36,7 @@ import FlexBetween from "../../components/FlexBeetween"
     const primary = palette.primary.main;
   
     const patchLike = async () => {
-      const response = await fetch(`http://localhost:3001/posts/${postId}/like`, {
+      const response = await fetch(`https://socialmediaapplication-gukz.onrender.com/posts/${postId}/like`, {
         method: "PATCH",
         headers: {
           Authorization: `Bearer ${token}`,
@@ -65,7 +65,7 @@ import FlexBetween from "../../components/FlexBeetween"
             height="auto"
             alt="post"
             style={{ borderRadius: "0.75rem", marginTop: "0.75rem" }}
-            src={`https://social-server-oe9l.onrender.com/assets/${picturePath}`}
+            src={`https://socialmediaapplication-gukz.onrender.com/assets/${picturePath}`}
           />
         )}
         <FlexBetween mt="0.25rem">
