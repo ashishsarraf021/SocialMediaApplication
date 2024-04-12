2d9b263e310246955451d6b55a19ca217a3c68c2
Ashish Sarraf <93375826+ashishsarraf021@users.noreply.github.com>
2024-04-12 22:41:56 +0530

    Update Friend.jsx

--- client/src/components/Friend.jsx
+++ client/src/components/Friend.jsx
@@ -23,7 +23,7 @@ const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
 
   const patchFriend = async () => {
     const response = await fetch(
-      `https://social-server-oe9l.onrender.com/users/${_id}/${friendId}`,
+      `https://socialmediaapplication-gukz.onrender.com/users/${_id}/${friendId}`,
       {
         method: "PATCH",
         headers: {
