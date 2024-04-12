b163605284fb61fc0780b7ea0a9f1640bab590c6
Ashish Sarraf <93375826+ashishsarraf021@users.noreply.github.com>
2024-04-12 22:38:14 +0530

    Update FriendListWidget.jsx

--- client/src/scenes/widgets/FriendListWidget.jsx
+++ client/src/scenes/widgets/FriendListWidget.jsx
@@ -13,7 +13,7 @@ const FriendListWidget = ({ userId }) => {
 
   const getFriends = async () => {
     const response = await fetch(
-      `https://social-server-oe9l.onrender.com/users/${userId}/friends`,
+      `https://socialmediaapplication-gukz.onrender.com/users/${userId}/friends`,
       {
         method: "GET",
         headers: { Authorization: `Bearer ${token}` },
