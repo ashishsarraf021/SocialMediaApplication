91c6e8a2222a69506c9d2717cd23afbd6b117f7f
Ashish Sarraf <93375826+ashishsarraf021@users.noreply.github.com>
2024-04-12 22:40:17 +0530

    Update PostsWidget.jsx

--- client/src/scenes/widgets/PostsWidget.jsx
+++ client/src/scenes/widgets/PostsWidget.jsx
@@ -9,7 +9,7 @@ const PostsWidget = ({ userId, isProfile = false }) => {
   const token = useSelector((state) => state.token);
 
   const getPosts = async () => {
-    const response = await fetch("http://localhost:3001/posts", {
+    const response = await fetch("https://socialmediaapplication-gukz.onrender.com/posts", {
       method: "GET",
       headers: { Authorization: `Bearer ${token}` },
     });
@@ -19,7 +19,7 @@ const PostsWidget = ({ userId, isProfile = false }) => {
 
   const getUserPosts = async () => {
     const response = await fetch(
-      `https://social-server-oe9l.onrender.com/posts/${userId}/posts`,
+      `https://socialmediaapplication-gukz.onrender.com/posts/${userId}/posts`,
       {
         method: "GET",
         headers: { Authorization: `Bearer ${token}` },
